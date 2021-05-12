d9678a83a84b26c6e30fa044f562aa86db22fdc6
sshoemaker <sshoemaker@protoplay.tv>
2021-05-12 19:21:50 -0400

    shrink assets and setup build

---
+++ build/static/js/53.46fc8386.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppersonal-app"]=this["webpackJsonppersonal-app"]||[]).push([[53],{148:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return r}));var o=e(17),i=e(13),r=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,r=document.elementFromPoint(t/2,e/2);if(r){var c=r.closest("ion-content");c&&new Promise((function(n){return Object(i.c)(c,n)})).then((function(){Object(o.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.46fc8386.chunk.js.map
