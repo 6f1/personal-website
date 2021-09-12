b68b27a35d6b94e9767f8f3e67cf6f56ad829b5c
sam <you@example.com>
2021-09-12 15:32:40 -0400

    fix

---
+++ build/static/js/53.4f7b7b53.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppersonal-app"]=this["webpackJsonppersonal-app"]||[]).push([[53],{149:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return r}));var o=e(17),i=e(13),r=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,r=document.elementFromPoint(t/2,e/2);if(r){var c=r.closest("ion-content");c&&new Promise((function(n){return Object(i.c)(c,n)})).then((function(){Object(o.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.4f7b7b53.chunk.js.map
