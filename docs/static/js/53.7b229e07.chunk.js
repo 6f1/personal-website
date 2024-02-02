8678786b8dae24ab34f84dc21c30f40afbde9c91
Sam Shoemaker <sshoemaker@protoplay.tv>
2024-02-02 12:17:28 -0500

    Structure changes

---
+++ docs/static/js/53.7b229e07.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppersonal-app"]=this["webpackJsonppersonal-app"]||[]).push([[53],{131:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return r}));var o=e(9),i=e(6),r=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,r=document.elementFromPoint(t/2,e/2);if(r){var c=r.closest("ion-content");c&&new Promise((function(n){return Object(i.c)(c,n)})).then((function(){Object(o.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.7b229e07.chunk.js.map
